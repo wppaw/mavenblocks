24a38156bb502fcb0dc429181c50d49e2cdd38ce
divipeoplehq <fahim@thinkrlab.com>
2023-03-08 18:14:35 +0600

    Initialized

---
+++ mavenblocks.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Plugin Name: Maven Blocks
+ * Plugin URI: https://mavenblocks.com
+ * Description: A collection of Gutenberg blocks for Maven
+ * Version: 1.0.0
+ * Author: WPPaw
+ * Author URI: https://mavenblocks.com
+ * License: GPL2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: mavenblocks
+ * Domain Path: /languages
+ * Requires at least: 5.0
+ * Requires PHP: 7.0
+ * Tested up to: 5.4
+ */
+
+// Exit if accessed directly.
+if (!defined('ABSPATH')) {
+    exit;
+}
